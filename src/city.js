e85c98168cd82701311482809f2f8ccdf2f2008b
KonovalovaVV <lerakonovalova1003@gmail.com>
2020-06-17 14:27:50 +0300

    Fix errors in test.js and city.js

--- src/city.js
+++ src/city.js
@@ -6,4 +6,6 @@ class City {
   }
 }
 
-export let data = [new City("Minsk", 20, 1), new City("Moskow", 100, 2), new City("Minsk", 30, 3)];
+const data = [new City('Minsk', 20, 1), new City('Moskow', 100, 2), new City('Minsk', 30, 3)];
+
+export default data;
--- src/test.js
+++ src/test.js
@@ -1,10 +1,10 @@
-import {data} from './city.js';
-import {firstBy} from './thenByFixed.js';
+import data from './city';
+import { firstBy } from './thenByFixed';
 
 data.sort(
-    firstBy(function (v1, v2) { return v1.name.length - v2.name.length; })
-    .thenBy(function (v1, v2) { return v1.population - v2.population; })
-    .thenBy(function (v1, v2) { return v1.id - v2.id; })
+  firstBy((v1, v2) => v1.name.length - v2.name.length)
+    .thenBy((v1, v2) => v1.population - v2.population)
+    .thenBy((v1, v2) => v1.id - v2.id),
 );
 
 console.log(data);
