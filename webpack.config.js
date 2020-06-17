c990d5234fb7772bd28dc2a0fadff309cd5da4fd
KonovalovaVV <lerakonovalova1003@gmail.com>
2020-06-17 14:37:43 +0300

    Fix errors in webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const path = require('path');
+
 const outputDirectory = path.join(__dirname, 'lib');
 const sourceDirectory = path.join(__dirname, 'src');
 
@@ -18,24 +19,24 @@ module.exports = {
   watch: true,
   module: {
     rules: [{
-        test: /\.html$/i,
-        loader: 'html-loader',
+      test: /\.html$/i,
+      loader: 'html-loader',
+    },
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
       },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
+    },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(sourceDirectory, 'testFixed.html')
+      template: path.join(sourceDirectory, 'testFixed.html'),
     }),
-  ]
+  ],
 };
